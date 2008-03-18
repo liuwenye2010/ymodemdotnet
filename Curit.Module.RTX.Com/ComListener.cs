2c3daf9070bea59a3fa46ad2bcf258101fbcb5e0
theajp01 <theajp01@ea84da9e-b248-0410-b7c7-b77a9d819ed5>
2008-03-18 09:30:42 +0000

    There's still some crap in there for the device I wrote it for but with some braincells and some knuckle grease you'll figure it out :) and even more

---
+++ Curit.Module.RTX.Com/ComListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curit.Module.RTX.Com
+{
+    public class ComListener
+    {
+        private SerialPort sp;
+
+        public Boolean update { get; private set; }
+        public Boolean binary { get; set; }
+
+        public void Start(SerialPort lsp)
+        {
+
+        }
+
+        
+    }
+}
